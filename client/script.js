0c6baed751f191a8b079d97d875eec94903b4d31
dovet <haebinlim@gmail.com>
2023-02-23 15:55:12 -0700

    Vercel server

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch response
-  const response = await fetch("https://codex-isxw.onrender.com/", {
+  const response = await fetch("https://chat-gpt-codex-sigma.vercel.app/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
